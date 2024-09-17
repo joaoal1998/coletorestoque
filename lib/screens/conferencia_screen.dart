20eff3b45f50d0af453c78666b12d6442e49c2f7
joaoal1998 <joaoal1998@gmail.com>
2024-09-17 17:26:43 -0300

    BottomNavigatorBar refactored

--- lib/screens/conferencia_screen.dart
+++ lib/screens/conferencia_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
-import 'ajustar_contagem_screen.dart';
-import 'consultar_estoque_screen.dart';
+import '../utils/navigator.dart';
 
 class ConferenciaScreen extends StatefulWidget {
   const ConferenciaScreen({super.key});
@@ -11,67 +9,20 @@ class ConferenciaScreen extends StatefulWidget {
 }
 
 class _ConferenciaScreenState extends State<ConferenciaScreen> {
-  int _selectedIndex = 2;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-
-    if (index == 0) {
-      Navigator.push(
-          context,
-          PageTransition(
-              child: const ConsultarEstoqueScreen(),
-              type: PageTransitionType.fade));
-    } else if (index == 1) {
-      Navigator.push(
-          context,
-          PageTransition(
-              child: const AjustarContagemScreen(),
-              type: PageTransitionType.fade));
-    } else if (index == 2) {
-      Navigator.push(
-          context,
-          PageTransition(
-              child: const ConferenciaScreen(),
-              type: PageTransitionType.fade));
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Conferência', textAlign: TextAlign.center),
-        backgroundColor: Colors.deepOrangeAccent,
-        foregroundColor: Colors.white,
-      ),
-      body: Container(
-        color: Colors.yellow[50],
-        child: const Center(
-          child: Text('Tela de Conferência'),
+        appBar: AppBar(
+          title: const Text('Conferência', textAlign: TextAlign.center),
+          backgroundColor: Colors.deepOrangeAccent,
+          foregroundColor: Colors.white,
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.deepOrangeAccent,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.inventory),
-            label: 'Estoque',
+        body: Container(
+          color: Colors.yellow[50],
+          child: const Center(
+            child: Text('Tela de Conferência'),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.edit),
-            label: 'Contagem',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.check_circle),
-            label: 'Conferência',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
+        ),
+        bottomNavigationBar: BottomNavigator());
   }
 }
--- lib/screens/consultar_estoque_screen.dart
+++ lib/screens/consultar_estoque_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
-import 'ajustar_contagem_screen.dart';
-import 'conferencia_screen.dart';
+import '../utils/navigator.dart';
 
 class ConsultarEstoqueScreen extends StatefulWidget {
   const ConsultarEstoqueScreen({super.key});
@@ -11,67 +9,20 @@ class ConsultarEstoqueScreen extends StatefulWidget {
 }
 
 class _ConsultarEstoqueScreenState extends State<ConsultarEstoqueScreen> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-
-    if (index == 0) {
-      Navigator.push(
-          context,
-          PageTransition(
-              child: const ConsultarEstoqueScreen(),
-              type: PageTransitionType.fade));
-    } else if (index == 1) {
-      Navigator.push(
-          context,
-          PageTransition(
-              child: const AjustarContagemScreen(),
-              type: PageTransitionType.fade));
-    } else if (index == 2) {
-      Navigator.push(
-          context,
-          PageTransition(
-              child: const ConferenciaScreen(),
-              type: PageTransitionType.fade));
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Consultar Estoque', textAlign: TextAlign.center),
-        backgroundColor: Colors.deepOrangeAccent,
-        foregroundColor: Colors.white,
-      ),
-      body: Container(
-        color: Colors.yellow[50],
-        child: const Center(
-          child: Text('Tela de Consulta de Estoque'),
+        appBar: AppBar(
+          title: const Text('Consultar Estoque', textAlign: TextAlign.center),
+          backgroundColor: Colors.deepOrangeAccent,
+          foregroundColor: Colors.white,
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.deepOrangeAccent,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.inventory),
-            label: 'Estoque',
+        body: Container(
+          color: Colors.yellow[50],
+          child: const Center(
+            child: Text('Tela de Consulta de Estoque'),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.edit),
-            label: 'Contagem',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.check_circle),
-            label: 'Conferência',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
+        ),
+        bottomNavigationBar: BottomNavigator());
   }
 }
